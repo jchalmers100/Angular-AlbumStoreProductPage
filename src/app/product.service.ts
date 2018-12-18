c5bf499452c71531a81c7066c84c29589050f446
james chalmers <james.chalmers@footballpools.com>
2018-12-18 12:13:07 +0000

    added files

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
@@ -9,8 +11,8 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
 }
